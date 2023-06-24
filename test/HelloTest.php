a91b585d839a884413e958b45cb13b82a8c5f08f
jvegalo <jmiguelv24@gmail.com>
2023-06-24 11:57:33 -0400

    test: test onMerge workflow

--- test/HelloTest.php
+++ test/HelloTest.php
@@ -1,6 +1,7 @@
 <?php
 
 class HelloTest extends \PHPUnit\Framework\TestCase {
+    
     /**
      * This test is a dummy test that always passes.
      * It was implemented just to test the Git Workflow
