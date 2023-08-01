e5f2006ced78c288b87ee4b1dee2b1c796a94711
Julian Ramirez <jramirezg@eafit.edu.co>
2023-08-01 13:04:40 -0500

    refactor: add try-catch blocks for errors handling

--- includes/model/Openedx_Woocommerce_Plugin_Logs.php
+++ includes/model/Openedx_Woocommerce_Plugin_Logs.php
@@ -29,35 +29,35 @@ class Openedx_Woocommerce_Plugin_Log {
      */
     public function createChangeLog( $post_id, $old_data_array, $enrollment_arr, $enrollment_action ) {
 
-        global $wpdb;
-        $tabla_logs = $wpdb->prefix . 'enrollment_logs_req_table';
-        $new_post = get_post($post_id);
-    
-        $date = current_time('mysql', true);
-        $user_id = get_current_user_id(); 
-        $username = get_user_by('id', $user_id)->user_login; 
-    
-        if (empty($old_data_array['enrollment_course_id'])) {
-            $enrollment_action = 'Object Created';
-        } 
-    
-        if (empty($old_data_array['enrollment_course_id'])) {
-            $old_data_array = '--';
-        }
-    
-        $log_data = array(
-            'post_id' => $post_id,
-            'mod_date' => $date,
-            'user' => $username,
-            'action_name' => $enrollment_action,
-            'object_before' => json_encode($old_data_array),
-            'object_after' => json_encode($enrollment_arr)
-        );
-    
-        $wpdb->insert($tabla_logs, $log_data);
-    
-        if ($wpdb->last_error) {
-            error_log('There was an error generating a register: ' . $wpdb->last_error);
+        try {
+            global $wpdb;
+            $tabla_logs = $wpdb->prefix . 'enrollment_logs_req_table';
+            $new_post = get_post($post_id);
+        
+            $date = current_time('mysql', true);
+            $user_id = get_current_user_id(); 
+            $username = get_user_by('id', $user_id)->user_login; 
+        
+            if (empty($old_data_array['enrollment_course_id'])) {
+                $enrollment_action = 'Object Created';
+            } 
+        
+            if (empty($old_data_array['enrollment_course_id'])) {
+                $old_data_array = '--';
+            }
+        
+            $log_data = array(
+                'post_id' => $post_id,
+                'mod_date' => $date,
+                'user' => $username,
+                'action_name' => $enrollment_action,
+                'object_before' => json_encode($old_data_array),
+                'object_after' => json_encode($enrollment_arr)
+            );
+        
+            $wpdb->insert($tabla_logs, $log_data);
+        } catch (Exception $e) {
+            error_log('An error occurred creating change log: ' . $e->getMessage());
         }
     }
 
@@ -70,27 +70,32 @@ class Openedx_Woocommerce_Plugin_Log {
     public function getLogs($post_id)
     {
         global $wpdb;
-        $tabla_logs = $wpdb->prefix . 'enrollment_logs_req_table';
+        $logs_table = $wpdb->prefix . 'enrollment_logs_req_table';
 
-        $logs = $wpdb->get_results(
-            $wpdb->prepare(
-                "SELECT * FROM $tabla_logs WHERE post_id = %d ORDER BY mod_date DESC",
-                $post_id
-            ),
-            ARRAY_A
-        );
+        try {
+            $logs = $wpdb->get_results(
+                $wpdb->prepare(
+                    "SELECT * FROM $logs_table WHERE post_id = %d ORDER BY mod_date DESC",
+                    $post_id
+                ),
+                ARRAY_A
+            );
 
-        $formatted_logs = '';
-        foreach ($logs as $log) {
-            $formatted_logs .= "<div class='log_entry'>";
-            $formatted_logs .= "<strong>Timestamp:</strong> " . date('d-m-Y H:i:s', strtotime($log['mod_date'])) . "<br>";
-            $formatted_logs .= "<strong>User:</strong> " . $log['user'] . "<br>";
-            $formatted_logs .= "<strong>Action:</strong> " . $log['action_name'] . "<br>";
-            $formatted_logs .= "<strong>Object Before:</strong> " . $log['object_before'] . "<br>";
-            $formatted_logs .= "<strong>Object After:</strong> " . $log['object_after'] . "<br>";
-            $formatted_logs .= "</div>";
+            $formatted_logs = '';
+            foreach ($logs as $log) {
+                $formatted_logs .= "<div class='log_entry'>";
+                $formatted_logs .= "<strong>Timestamp:</strong> " . date('d-m-Y H:i:s', strtotime($log['mod_date'])) . "<br>";
+                $formatted_logs .= "<strong>User:</strong> " . $log['user'] . "<br>";
+                $formatted_logs .= "<strong>Action:</strong> " . $log['action_name'] . "<br>";
+                $formatted_logs .= "<strong>Object Before:</strong> " . $log['object_before'] . "<br>";
+                $formatted_logs .= "<strong>Object After:</strong> " . $log['object_after'] . "<br>";
+                $formatted_logs .= "</div>";
+            }
+
+            return $formatted_logs;
+        } catch (Exception $e) {
+            error_log('An error occurred while fetching logs: ' . $e->getMessage());
+            return 'Error occurred while fetching logs. Please try again later.';
         }
-
-        return $formatted_logs;
     }
 }
