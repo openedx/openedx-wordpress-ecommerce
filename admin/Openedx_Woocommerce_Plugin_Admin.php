c61fde2015f1de74718e9bd6c4e37245f0d40f05
Julián Ramirez Giraldo <52968528+julianramirez2@users.noreply.github.com>
2023-08-02 12:25:39 -0500

    fix: course mode description improved
    Co-authored-by: María Fernanda Magallanes <35668326+MaferMazu@users.noreply.github.com>

--- admin/Openedx_Woocommerce_Plugin_Admin.php
+++ admin/Openedx_Woocommerce_Plugin_Admin.php
@@ -217,7 +217,7 @@ class Openedx_Woocommerce_Plugin_Admin
 			'id'          => '_mode',
 			'label'       => __('Open edX Course Mode', 'woocommerce'),
 			'desc_tip'    => 'true',
-			'description' => __('Select the mode for the product.', 'woocommerce'),
+			'description' => __('Select the mode for your course. Make sure to set a mode that your course has.', 'woocommerce'),
 			'options'     => array(
 				'Honor'             => __('Honor', 'woocommerce'),
 				'Audit'             => __('Audit', 'woocommerce'),
