594cd312506c1291a370f7653fbb299a1415e0fa
Julián Ramirez Giraldo <52968528+julianramirez2@users.noreply.github.com>
2023-10-10 15:35:25 -0500

    fix: access token doesn't generate errors when token expires (#45)
    * fix: access token doesn't generate errors when token expires
    
    * refactor: deleted unnecessary spaces
    
    * docs: added extra info

--- includes/model/class-openedx-woocommerce-plugin-api-calls.php
+++ includes/model/class-openedx-woocommerce-plugin-api-calls.php
@@ -127,7 +127,13 @@ class Openedx_Woocommerce_Plugin_Api_Calls {
 		}
 
 		$access_token = $this->check_access_token();
-		$user         = $this->get_user( $enrollment_data['enrollment_email'], $access_token );
+
+		// The access token can be an array or not; if it's an array, we need the value at index 1, which contains the generated token.
+		if ( 'array' === gettype( $access_token ) ) {
+			$access_token = $access_token[1];
+		}
+
+		$user = $this->get_user( $enrollment_data['enrollment_email'], $access_token );
 
 		if ( 'error' === $access_token[0] || 'error' === $user[0] ) {
 			if ( 'error' === $access_token[0] && 'error' === $user[0] ) {
